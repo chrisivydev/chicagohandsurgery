a2703c7c944271e725bbc97fac338b06c4d235e1
chrisivydev <cdi.747@gmail.com>
2025-06-20 14:49:53 -0700

    Build: Temporarily disable routing for debug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,22 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+// import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+// import NotFound from "@/pages/not-found";
 import Landing from "@/pages/Landing";
-import Home from "@/pages/Home";
-import About from "@/pages/About";
-import Members from "@/pages/Members";
-import Events from "@/pages/Events";
-import News from "@/pages/News";
-import Contact from "@/pages/Contact";
-import Login from "@/pages/Login";
-import { useAuth } from "@/hooks/useAuth";
+// import Home from "@/pages/Home";
+// import About from "@/pages/About";
+// import Members from "@/pages/Members";
+// import Events from "@/pages/Events";
+// import News from "@/pages/News";
+// import Contact from "@/pages/Contact";
+// import Login from "@/pages/Login";
+// import { useAuth } from "@/hooks/useAuth";
 
-const base = import.meta.env.PROD ? "/chicagohandsurgery" : "";
+// const base = import.meta.env.PROD ? "/chicagohandsurgery" : "";
 
+/*
 function Router() {
   const { isAuthenticated, isLoading } = useAuth();
 
@@ -47,13 +48,14 @@ function Router() {
     </WouterRouter>
   );
 }
+*/
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Landing />
       </TooltipProvider>
     </QueryClientProvider>
   );
